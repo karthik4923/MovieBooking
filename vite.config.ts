6982b2fb7d4a1d7d34eca2ee1a5ed0ae0c746108
karthik4923 <93390048+karthik4923@users.noreply.github.com>
2026-07-09 13:19:16 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import plugin from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [plugin()],
+    base: '/MovieBooking/'
     server: {
         port: 54343,
     }
